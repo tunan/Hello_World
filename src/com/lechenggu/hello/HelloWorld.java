a1423d4a6f1d841459879c22deb74ac50f6098dd
tunan <taolincha@163.com>
2017-03-30 15:12:13 +0800

    Initial commit

---
+++ src/com/lechenggu/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.lechenggu.hello;
+
+/**
+ * Created by suduo on 2017/3/29.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+
+}
